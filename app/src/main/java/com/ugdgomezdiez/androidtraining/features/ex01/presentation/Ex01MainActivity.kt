6f2d784e81a43ba3a9dfe9015b12ed7e8781f3b5
Unai-Gomez-Diez <Unaigomez0807@gmail.com>
2023-10-10 16:11:49 +0200

    haciendo el listado

--- app/src/main/java/com/ugdgomezdiez/androidtraining/features/ex01/presentation/Ex01MainActivity.kt
+++ app/src/main/java/com/ugdgomezdiez/androidtraining/features/ex01/presentation/Ex01MainActivity.kt
@@ -3,8 +3,10 @@ package com.ugdgomezdiez.androidtraining.features.ex01.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.lifecycle.Observer
 import com.ugdgomezdiez.androidtraining.R
 import com.ugdgomezdiez.androidtraining.features.ex01.data.UserDataRepository
@@ -30,36 +32,17 @@ class Ex01MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_formulario)
         setupView()
         setupObservers()
-        val editText = findViewById<EditText>(R.id.name)
-        editText.visibility = View.INVISIBLE
-        val editText2 = findViewById<EditText>(R.id.surname)
-        editText2.visibility = View.INVISIBLE
-        val myButton = findViewById<Button>(R.id.action_clean)
-        myButton.visibility = View.INVISIBLE
-        val editText3 = findViewById<EditText>(R.id.name2)
-        editText3.visibility = View.INVISIBLE
-        val editText4 = findViewById<EditText>(R.id.surname2)
-        editText4.visibility = View.INVISIBLE
-        val myButton2 = findViewById<Button>(R.id.action_clean2)
-        myButton2.visibility = View.INVISIBLE
-        val editText5 = findViewById<EditText>(R.id.name3)
-        editText5.visibility = View.INVISIBLE
-        val editText6 = findViewById<EditText>(R.id.surname3)
-        editText6.visibility = View.INVISIBLE
-        val myButton3 = findViewById<Button>(R.id.action_clean3)
-        myButton3.visibility = View.INVISIBLE
-        val editText7 = findViewById<EditText>(R.id.name4)
-        editText7.visibility = View.INVISIBLE
-        val editText8 = findViewById<EditText>(R.id.surname4)
-        editText8.visibility = View.INVISIBLE
-        val myButton4 = findViewById<Button>(R.id.action_clean4)
-        myButton4.visibility = View.INVISIBLE
-        val editText9 = findViewById<EditText>(R.id.name5)
-        editText9.visibility = View.INVISIBLE
-        val editText10 = findViewById<EditText>(R.id.surname5)
-        editText10.visibility = View.INVISIBLE
-        val myButton5 = findViewById<Button>(R.id.action_clean5)
-        myButton5.visibility = View.INVISIBLE
+        val viewGroup1 = findViewById<ViewGroup>(R.id.row_1)
+        viewGroup1.visibility=View.GONE
+        val viewGroup2 = findViewById<ViewGroup>(R.id.row_2)
+        viewGroup2.visibility=View.GONE
+        val viewGroup3 = findViewById<ViewGroup>(R.id.row_3)
+        viewGroup3.visibility=View.GONE
+        val viewGroup4 = findViewById<ViewGroup>(R.id.row_4)
+        viewGroup4.visibility=View.GONE
+        val viewGroup5 = findViewById<ViewGroup>(R.id.row_5)
+        viewGroup5.visibility=View.GONE
+
         //viewModel.loadUser()
     }
 
@@ -70,36 +53,8 @@ class Ex01MainActivity : AppCompatActivity() {
         actionButton.setOnClickListener {
             viewModel.saveUser(getNameInput(),getSurnameInput())
             viewModel.loadUser()
-            val editText = findViewById<EditText>(R.id.name)
-            editText.visibility = View.VISIBLE
-            val editText2 = findViewById<EditText>(R.id.surname)
-            editText2.visibility = View.VISIBLE
-            val myButton = findViewById<Button>(R.id.action_clean)
-            myButton.visibility = View.VISIBLE
-            val editText3 = findViewById<EditText>(R.id.name2)
-            editText3.visibility = View.VISIBLE
-            val editText4 = findViewById<EditText>(R.id.surname2)
-            editText4.visibility = View.VISIBLE
-            val myButton2 = findViewById<Button>(R.id.action_clean2)
-            myButton2.visibility = View.VISIBLE
-            val editText5 = findViewById<EditText>(R.id.name3)
-            editText5.visibility = View.VISIBLE
-            val editText6 = findViewById<EditText>(R.id.surname3)
-            editText6.visibility = View.VISIBLE
-            val myButton3 = findViewById<Button>(R.id.action_clean3)
-            myButton3.visibility = View.VISIBLE
-            val editText7 = findViewById<EditText>(R.id.name4)
-            editText7.visibility = View.VISIBLE
-            val editText8 = findViewById<EditText>(R.id.surname4)
-            editText8.visibility = View.VISIBLE
-            val myButton4 = findViewById<Button>(R.id.action_clean4)
-            myButton4.visibility = View.VISIBLE
-            val editText9 = findViewById<EditText>(R.id.name5)
-            editText9.visibility = View.VISIBLE
-            val editText10 = findViewById<EditText>(R.id.surname5)
-            editText10.visibility = View.VISIBLE
-            val myButton5 = findViewById<Button>(R.id.action_clean5)
-            myButton5.visibility = View.VISIBLE
+
+            findViewById<ViewGroup>(R.id.row_1).findViewById<TextView>(R.id.name)
         }
 
         val actionButton2 = findViewById<Button>(R.id.action_get)
@@ -108,66 +63,16 @@ class Ex01MainActivity : AppCompatActivity() {
             findViewById<EditText>(R.id.input_surname).setText("")
         }
 
-        val actionButton3 = findViewById<Button>(R.id.action_clean)
+        val actionButton3 = findViewById<ViewGroup>(R.id.row_1).findViewById<Button>(R.id.action_clean)
         actionButton3.setOnClickListener {
             viewModel.resetUser()
 
-            val editText = findViewById<EditText>(R.id.name)
-            editText.visibility = View.INVISIBLE
-            val editText2 = findViewById<EditText>(R.id.surname)
-            editText2.visibility = View.INVISIBLE
-            val myButton = findViewById<Button>(R.id.action_clean)
-            myButton.visibility = View.INVISIBLE
+            val viewGroup = findViewById<ViewGroup>(R.id.row_1)
+            viewGroup.visibility=View.GONE
+
 
         }
-        val actionButton4 = findViewById<Button>(R.id.action_clean2)
-        actionButton4.setOnClickListener {
-            viewModel.resetUser()
 
-            val editText = findViewById<EditText>(R.id.name2)
-            editText.visibility = View.INVISIBLE
-            val editText2 = findViewById<EditText>(R.id.surname2)
-            editText2.visibility = View.INVISIBLE
-            val myButton = findViewById<Button>(R.id.action_clean2)
-            myButton.visibility = View.INVISIBLE
-
-        }
-        val actionButton5 = findViewById<Button>(R.id.action_clean3)
-        actionButton5.setOnClickListener {
-            viewModel.resetUser()
-
-            val editText = findViewById<EditText>(R.id.name3)
-            editText.visibility = View.INVISIBLE
-            val editText2 = findViewById<EditText>(R.id.surname3)
-            editText2.visibility = View.INVISIBLE
-            val myButton = findViewById<Button>(R.id.action_clean3)
-            myButton.visibility = View.INVISIBLE
-
-        }
-        val actionButton6 = findViewById<Button>(R.id.action_clean4)
-        actionButton6.setOnClickListener {
-            viewModel.resetUser()
-
-            val editText = findViewById<EditText>(R.id.name4)
-            editText.visibility = View.INVISIBLE
-            val editText2 = findViewById<EditText>(R.id.surname4)
-            editText2.visibility = View.INVISIBLE
-            val myButton = findViewById<Button>(R.id.action_clean4)
-            myButton.visibility = View.INVISIBLE
-
-        }
-        val actionButton7 = findViewById<Button>(R.id.action_clean5)
-        actionButton7.setOnClickListener {
-            viewModel.resetUser()
-
-            val editText = findViewById<EditText>(R.id.name5)
-            editText.visibility = View.INVISIBLE
-            val editText2 = findViewById<EditText>(R.id.surname5)
-            editText2.visibility = View.INVISIBLE
-            val myButton = findViewById<Button>(R.id.action_clean5)
-            myButton.visibility = View.INVISIBLE
-
-        }
     }
 
    private fun getNameInput(): String=
