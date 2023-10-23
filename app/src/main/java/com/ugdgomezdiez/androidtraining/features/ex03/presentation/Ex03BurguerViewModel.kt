2e4b832cc73803d99125b293489c22d3f5771e59
Unai-Gomez-Diez <Unaigomez0807@gmail.com>
2023-10-23 20:35:14 +0200

    haciendo burguer tambien pero mejor

--- app/src/main/java/com/ugdgomezdiez/androidtraining/features/ex03/presentation/Ex03BurguerViewModel.kt
+++ app/src/main/java/com/ugdgomezdiez/androidtraining/features/ex03/presentation/Ex03BurguerViewModel.kt
@@ -1,4 +1,30 @@
 package com.ugdgomezdiez.androidtraining.features.ex03.presentation
 
-class Ex03BurguerViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ugdgomezdiez.androidtraining.app.ErrorApp
+import com.ugdgomezdiez.androidtraining.features.ex03.domain.Burguer
+import com.ugdgomezdiez.androidtraining.features.ex03.domain.GetBurguerUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class Ex03BurguerViewModel(private val getBurguerUseCase: GetBurguerUseCase): ViewModel() {
+
+    private val _uiModel = MutableLiveData<UiModel>()
+    val uiModel: LiveData<UiModel> = _uiModel
+
+    fun loadBurguer(){
+        _uiModel.value = UiModel(isLoading = true)
+
+        viewModelScope.launch(Dispatchers.IO) {  }
+    }
+
+
+            data class UiModel(
+                val isLoading: Boolean= false,
+                val errorApp: ErrorApp?=null,
+                val burguer: Burguer?=null
+            )
 }
